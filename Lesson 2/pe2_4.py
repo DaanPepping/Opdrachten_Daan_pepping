db61525503ba0925f551934c7e0d620722c113ad
Daan Pepping <daan.pepping@student.hu.nl>
2017-09-07 12:00:22 +0200

    werkende versie

---
+++ Lesson 2/pe2_4.py
@@ -0,0 +1,6 @@
+uurloon = input('wat verdien je per uur:' )
+uren = input('hoeveel uur heb je gewerkt:' )
+loon = int(uurloon) * int(uren)
+eindsom = str(uren) + ' uur werken levert ' + str(loon) + ' Euro op!'
+print(eindsom)
+
